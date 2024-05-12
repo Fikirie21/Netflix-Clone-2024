f3a4741dfcb301d95651db3d1f4d88fe11613080
FikirMersha <162644747+FikirMersha@users.noreply.github.com>
2024-05-11 22:46:49 -0400

    row

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from "../../Component/Header/Header";
 import Footer from "../../Component/Footer/Footer";
+import Banner from "../../Component/Banner/Banner";
+import RowList from "../../Component/Rows/RowList/RowList"
 function Home() {
   return (
     <>
         <Header/>
+        <Banner/>
+        <RowList/>
         <Footer/>
     </>
   )
--- netflix-clone/src/Utils/Axios.js
+++ netflix-clone/src/Utils/Axios.js
@@ -1,6 +1,6 @@
-import Axios from "Axios";
-const instance =Axios.create({
-    baseURL:"https://api.themoviedb.org/3",
+import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
 });
 export default instance;
